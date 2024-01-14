a807a2c53866af012b70a17f7087cc720b03b7aa
victorrlo <42052096+victorrlo@users.noreply.github.com>
2024-01-14 12:36:00 -0300

    Problema de volume resolvido
    O step não pode ser "any", precisa ser definido um valor.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let mapedKeys = [];
 let audio = new Audio("src/tunes/StatusRecovery.wav");
+const volumeSlider = document.querySelector(".volume-slider input");
 
 const playTune = (key) => {
     audio.play();
@@ -15,11 +16,18 @@ const playTune = (key) => {
 pianoKeys.forEach((key)=>{
     key.addEventListener("click",()=> playTune(key.dataset.key));
     mapedKeys.push(key.dataset.key);
-})
+});
 
 document.addEventListener("keydown", (e)=>{
     if(mapedKeys.includes(e.key)){
         playTune(e.key); 
     }
 
-})
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+    console.log(e.target.value);
+}
+
+volumeSlider.addEventListener("input", handleVolume);
