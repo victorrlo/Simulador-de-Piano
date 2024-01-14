325276e9b5a87921d1468591165b8bd68588cfc9
victorrlo <42052096+victorrlo@users.noreply.github.com>
2024-01-14 12:26:06 -0300

    Apenas teclas mapeadas para tocar som

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,10 +1,25 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let mapedKeys = [];
 let audio = new Audio("src/tunes/StatusRecovery.wav");
 
 const playTune = (key) => {
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=>{
+        clickedKey.classList.remove("active");
+    }, 150);
 }
 
 pianoKeys.forEach((key)=>{
-    key.addEventListener("click",()=> playTune(key.CDATA_SECTION_NODE.key));
+    key.addEventListener("click",()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+})
+
+document.addEventListener("keydown", (e)=>{
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key); 
+    }
+
 })
